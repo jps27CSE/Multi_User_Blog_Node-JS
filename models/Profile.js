933aad4b0271b89d41f4460b9fe12a2d7de88d61
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-10-25 12:49:51 +0600

    Profile Model

---
+++ models/Profile.js
@@ -0,0 +1,48 @@
+// user , title ,bio , profilePics, links : {fb,twiiter,website}
+// posts, bookmarks
+
+const { Schema, model } = require("mongoose");
+
+const profileSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: string,
+      trim: true,
+      maxlength: 100,
+    },
+    bio: {
+      type: string,
+      trim: true,
+      maxlength: 500,
+    },
+    profilePic: String,
+    links: {
+      website: string,
+      facebook: string,
+      twitter: string,
+      github: string,
+    },
+    posts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+    bookmarks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Profile = model("Profile", profileSchema);
+
+module.exports = Profile;
