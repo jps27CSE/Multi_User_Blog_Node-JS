485fc0e7bee763a7c3b05e5d995b922e57d93aee
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-10-26 01:10:29 +0600

    auth Controller route impliment

---
+++ controllers/authController.js
@@ -0,0 +1,5 @@
+exports.signupGetController = (req, res, next) => {};
+exports.signupPostController = (req, res, next) => {};
+exports.loginGetController = (req, res, next) => {};
+exports.loginPostController = (req, res, next) => {};
+exports.logoutController = (req, res, next) => {};
