05cfa687d00671d40a53bb3bcc09505c968a9716
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-10-26 01:21:25 +0600

    Setup App For EJS

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const middleware = [
+  morgan("dev"),
+  express.static("public"),
+  express.urlencoded({ extended: true }),
+  express.json(),
+];
+
+app.use(middleware);
 
 app.get("/", (req, res) => {
   res.json({
