6cdf11a72b77ff7b1299efbd96635a84891f1441
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-10-26 19:23:28 +0600

    create playground for validator

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const { URL } = require("./url");
 const authRoutes = require("./routes/authRoute");
 const mongoose = require("mongoose");
 
+//Plyaground Routes
+const validatorRoutes = require("./playground/validator");
+
 app.set("view engine", "ejs");
 app.set("views", "views");
 
@@ -18,6 +21,9 @@ const middleware = [
 app.use(middleware);
 
 app.use("/auth", authRoutes);
+
+//Playground
+app.use("/playground", validatorRoutes);
 
 app.get("/", (req, res) => {
   res.json({
