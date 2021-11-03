c390f52beac0404138b05763b5ad0f856c52ed7a
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-11-04 01:00:45 +0600

    Setup Storage

---
+++ middleware/uploadMiddleware.js
@@ -0,0 +1,11 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "-" + Date.now() + "-" + file.originalname);
+  },
+});
