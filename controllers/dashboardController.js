c16c418593b1662040443793ed9a50f5e5460fb0
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-11-04 19:19:14 +0600

    Conditionally Render Dashboard or Profile

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,8 +1,33 @@
 const Flash = require("../utils/Flash");
+const Profile = require("../models/Profile");
 
-exports.dashboardGetController = (req, res, next) => {
-  res.render("pages/dashboard/dashboard", {
-    title: "My Dashboard",
-    flashMessage: Flash.getMessage(req),
-  });
+exports.dashboardGetController = async (req, res, next) => {
+  try {
+    let profile = await Profile.findOne({ user: req.user._id });
+    if (profile) {
+      return res.render("pages/dashboard/dashboard", {
+        title: "My Dashboard",
+        flashMessage: Flash.getMessage(req),
+      });
+    }
+
+    res.redirect("/dashboard/create-profile");
+  } catch (e) {
+    next(e);
+  }
+};
+
+exports.createProfileGetController = async (req, res, next) => {
+  try {
+    let profile = await Profile.findOne({ user: req.user._id });
+    if (profile) {
+      return res.redirect("/dashboard/edit-profile");
+    }
+    res.render("pages/dashboard/create-profile", {
+      title: "Create Your Profile",
+      flashMessage: Flash.getMessage(req),
+    });
+  } catch (e) {
+    next(e);
+  }
 };
