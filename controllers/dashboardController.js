b650e3019a53e6b1e3278fe90c9fe657bc0ba925
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-11-08 19:55:06 +0600

    Save Profile Data

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -2,6 +2,7 @@ const Flash = require("../utils/Flash");
 const Profile = require("../models/Profile");
 const { validationResult } = require("express-validator");
 const errorFormatter = require("../utils/validationErrorFormatter");
+const User = require("../models/User");
 
 exports.dashboardGetController = async (req, res, next) => {
   try {
@@ -35,7 +36,7 @@ exports.createProfileGetController = async (req, res, next) => {
   }
 };
 
-exports.createProfilePostController = (req, res, next) => {
+exports.createProfilePostController = async (req, res, next) => {
   let errors = validationResult(req).formatWith(errorFormatter);
 
   if (!errors.isEmpty()) {
@@ -46,11 +47,34 @@ exports.createProfilePostController = (req, res, next) => {
     });
   }
 
-  res.render("pages/dashboard/create-profile", {
-    title: "Create Your Profile",
-    flashMessage: Flash.getMessage(req),
-    error: {},
-  });
+  let { name, title, bio, website, facebook, twiiter, github } = req.body;
+
+  try {
+    let profile = new Profile({
+      user: req.user._id,
+      name,
+      title,
+      bio,
+      profilePics: req.user.profilePics,
+      links: {
+        website: website || "",
+        facebook: facebook || "",
+        github: github || "",
+      },
+      posts: [],
+      bookmarks: [],
+    });
+
+    let createdProfile = await profile.save();
+    await User.findOneAndUpdate(
+      { _id: req.user._id },
+      { $set: { profile: createdProfile._id } }
+    );
+    req.flash("sucess", "Profile Created Successfully");
+    res.redirect("/dashboard");
+  } catch (e) {
+    next(e);
+  }
 };
 
 exports.editProfileGetController = (req, res, next) => {
