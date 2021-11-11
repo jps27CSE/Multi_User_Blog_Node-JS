b0b6b7c25c5cd8fbafefe48f3f7a5e47a6758d4c
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-11-11 22:51:38 +0600

    Error Handling

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,3 @@
-const Flash = require("../utils/Flash");
-
 const { validationResult } = require("express-validator");
 const Flash = require("../utils/Flash");
 const errorFormatter = require("../utils/validationErrorFormatter");
@@ -9,16 +7,29 @@ exports.createPostGetController = (req, res, next) => {
     title: "Create A New Post",
     error: {},
     flashMessage: Flash.getMessage(req),
+    value: {},
   });
 };
 
 exports.createPostPostController = (req, res, next) => {
+  let { title, body, tags } = req.body;
+
   let errors = validationResult(req).formatWith(errorFormatter);
   console.log(errors);
+
+  if (!errors.isEmpty()) {
+    res.render("pages/dashboard/post/createPost", {
+      title: "Create A New Post",
+      error: errors.mapped(),
+      flashMessage: Flash.getMessage(req),
+      value: { title, body, tags },
+    });
+  }
 
   res.render("pages/dashboard/post/createPost", {
     title: "Create A New Post",
     error: {},
     flashMessage: Flash.getMessage(req),
+    value: {},
   });
 };
